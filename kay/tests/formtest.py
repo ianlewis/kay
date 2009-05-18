f714240a63800bdb59e067fa2bdcb0d6018ae1df
Takashi Matsuo <tmatsuo@candit.jp>
2009-05-18 23:32:40 +0900

    Added base test class.

--- kay/tests/formtest.py
+++ kay/tests/formtest.py
@@ -18,43 +18,14 @@ APP_ID = u'test'
 os.environ['APPLICATION_ID'] = APP_ID
 
 from google.appengine.ext import db
-from google.appengine.api import apiproxy_stub_map
-from google.appengine.api import datastore_file_stub
-from google.appengine.api import mail_stub
-from google.appengine.api import urlfetch_stub
-from google.appengine.api.memcache import memcache_stub
-from google.appengine.api import user_service_stub
-
 import kay
 from werkzeug import Request
 
 from kay.utils import local
 from kay.utils import forms
+from kay.utils.forms import ValidationError
 
-class GAETestBase(unittest.TestCase):
-
-  def get_env(self):
-    env = dict(os.environ)
-    env['wsgi.input'] = sys.stdin
-    env['wsgi.errors'] = sys.stderr
-    env['wsgi.version'] = "1.0"
-    env['wsgi.run_once'] = True
-    env['wsgi.url_scheme'] = 'http'
-    env['wsgi.multithread']  = False
-    env['wsgi.multiprocess'] = True
-    return env
-    
-  def setUp(self):
-    apiproxy_stub_map.apiproxy = apiproxy_stub_map.APIProxyStubMap()
-    stub = datastore_file_stub.DatastoreFileStub('test','/dev/null',
-                                                 '/dev/null')
-    apiproxy_stub_map.apiproxy.RegisterStub('datastore_v3', stub)
-
-    apiproxy_stub_map.apiproxy.RegisterStub(
-      'user', user_service_stub.UserServiceStub())
-
-    apiproxy_stub_map.apiproxy.RegisterStub(
-      'memcache', memcache_stub.MemcacheServiceStub())
+from base import GAETestBase
 
 class TestModel(db.Model):
   number = db.IntegerProperty()
@@ -67,7 +38,8 @@ class TestForm(forms.Form):
   password = forms.TextField("password", required=True)
   password_again = forms.TextField("confirm password", required=True)
   model_field = forms.ModelField(TestModel, label="ModelField Test",
-                                 required=True, filter=('is_active =', True),
+                                 required=True,
+                                 query=TestModel.all().filter('is_active =', True),
                                  option_name='data')
   
   def context_validate(self, data):
@@ -83,18 +55,23 @@ class FormTest(GAETestBase):
         t.put()
 
   def test_form(self):
+    """Form validation test with context_validate."""
     os.environ['REQUEST_METHOD'] = 'POST'
     local.request = Request(self.get_env())
     f = TestForm()
-    self.assertEqual(f.validate({'username': 'hoge'}), False)
-    valid_params = {
+    params = {'username': 'hoge'}
+    self.assertEqual(f.validate(params), False)
+    params = {
       'username': 'hoge',
       'password': 'fugafuga',
       'password_again': 'fugafuga',
       'model_field': str(TestModel.all().get().key())
     }
-    result = f.validate(valid_params)
+    result = f.validate(params)
     self.assertEqual(result, True)
+    params['password_again'] = 'moge'
+    result = f.validate(params)
+    self.assertEqual(result, False)
 
   def tearDown(self):
     entries = TestModel.all().fetch(100)
@@ -102,39 +79,69 @@ class FormTest(GAETestBase):
 
 class TestForm2(forms.Form):
   csrf_protected = False
+  int_field = forms.IntegerField("int", min_value=5, max_value=99)
   float_field = forms.FloatField("float", min_value=5.5, max_value=99.9)
+  number_field = forms.NumberField("number", min_value=5.5, max_value=99.9)
 
-class FloatFieldTest(GAETestBase):
+class NumberFieldTest(GAETestBase):
   def setUp(self):
-    super(FloatFieldTest, self).setUp()
+    super(NumberFieldTest, self).setUp()
     os.environ['REQUEST_METHOD'] = 'POST'
     local.request = Request(self.get_env())
 
-  def test_validate_float(self):
-    """
-    Float value validation test.
-    """
+  def test_validate(self):
+    """Float value validation test."""
     f = TestForm2()
     result = f.validate({'float_field': 10.7})
     self.assertEqual(result, True)
+    self.assertEqual(f['float_field'], 10.7)
+
+    f = TestForm2()
     result = f.validate({'float_field': 'ten'})
     self.assertEqual(result, False)
 
+    f = TestForm2()
+    result = f.validate({'number_field': 10.7})
+    self.assertEqual(result, True)
+    self.assertEqual(f['number_field'], 10.7)
+
+    f = TestForm2()
+    result = f.validate({'number_field': 'ten'})
+    self.assertEqual(result, False)
+
+    f = TestForm2()
+    result = f.validate({'int_field': 10})
+    self.assertEqual(result, True)
+    self.assertEqual(f['int_field'], 10)
+
+    f = TestForm2()
+    result = f.validate({'int_field': 'ten'})
+    self.assertEqual(result, False)
+
   def test_min(self):
-    """
-    Minimal value validation test.
-    """
+    """Minimal value validation test."""
     f = TestForm2()
     result = f.validate({'float_field': 5.4})
     self.assertEqual(result, False)
 
+    result = f.validate({'int_field': 4})
+    self.assertEqual(result, False)
+
+    result = f.validate({'number_field': 5.4})
+    self.assertEqual(result, False)
+
   def test_max(self):
-    """
-    Maximum value validation test.
-    """
+    """Maximum value validation test."""
     f = TestForm2()
     result = f.validate({'float_field': 100.1})
     self.assertEqual(result, False)
+
+    result = f.validate({'int_field': 100})
+    self.assertEqual(result, False)
+
+    result = f.validate({'number_field': 100.1})
+    self.assertEqual(result, False)
+
 
 if __name__ == "__main__":
   unittest.main()
