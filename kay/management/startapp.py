a054dfc5b695a39a19dcae547a62c70a2ecc29f8
Takashi Matsuo <tmatsuo@candit.jp>
2009-05-21 11:07:24 +0900

    Moved management scripts into kay/management.

---
+++ kay/management/startapp.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def startapp():
+  """
+  Start new application.
+  """
+  print "startapp"
--- manage.py
+++ manage.py
@@ -9,12 +9,14 @@ sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 import kay
 kay.setup_env(manage_py_env=True)
 from werkzeug import script
-from kay.misc.script import make_remote_shell, init_remote_shell
-from kay.misc.runserver import start_dev_appserver
+from kay.management.rshell import make_remote_shell, init_remote_shell
+from kay.management.runserver import start_dev_appserver
+from kay.management.startapp import startapp
 
 action_shell = script.make_shell()
 action_rshell = make_remote_shell(init_remote_shell)
 action_runserver = start_dev_appserver
+action_startapp = startapp
 
 if __name__ == '__main__':
   script.run()
