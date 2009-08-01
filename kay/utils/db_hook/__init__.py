c157ef1502f4bceb6551171739fc52852649d377
Takashi Matsuo <tmatsuo@candit.jp>
2009-08-01 23:43:22 +0900

    Updated README. Changed db_hook to a package.
    --HG--
    branch : tmatsuo
    rename : kay/utils/db_hook.py => kay/utils/db_hook/__init__.py

--- kay/utils/db_hook/__init__.py
+++ kay/utils/db_hook/__init__.py
@@ -10,13 +10,10 @@ Kay utils.db_hook module.
 from google.appengine.api import datastore
 from google.appengine.ext import db
 
+import put_type
+
 post_save_hooks = {}
 
-NEWLY_CREATED = 1
-UPDATED = 2
-MAYBE_NEWLY_CREATED = 3
-MAYBE_UPDATED = 4
-UNKOWN = 5
 
 def register_post_save_hook(func, model):
   global post_save_hooks
@@ -28,16 +25,16 @@ def register_post_save_hook(func, model):
   post_save_hooks[kind] = func_list
 
 def execute_hooks(kind, key, entity):
-  put_type = UNKOWN
+  put_type_id = put_type.UNKOWN
   func_list = post_save_hooks.get(kind, None)
   if func_list is not None:
     last_path = entity.key().path().element_list()[-1]
     has_name = last_path.has_name()
     if last_path.has_id():
       if last_path.id() == 0:
-        put_type = NEWLY_CREATED
+        put_type_id = put_type.NEWLY_CREATED
       else:
-        put_type = UPDATED
+        put_type_id = put_type.UPDATED
     entity.key_.CopyFrom(key)
     e = datastore.Entity._FromPb(entity)
     instance = db.class_for_kind(kind).from_entity(e)
@@ -47,13 +44,13 @@ def execute_hooks(kind, key, entity):
         threshold = datetime.timedelta(0,0,1000)
         if hasattr(instance, "updated"):
           if abs(instance.created - instance.updated) < threshold:
-            put_type = MAYBE_NEWLY_CREATED
+            put_type_id = put_type.MAYBE_NEWLY_CREATED
           else:
-            put_type = MAYBE_UPDATED
+            put_type_id = put_type.MAYBE_UPDATED
         else:
           if (datetime.datetime.now() - instance.created) < threshold:
-            put_type = MAYBE_NEWLY_CREATED
+            put_type_id = put_type.MAYBE_NEWLY_CREATED
           else:
-            put_type = MAYBE_UPDATED
+            put_type_id = put_type.MAYBE_UPDATED
     for func in func_list:
-      func(instance, put_type)
+      func(instance, put_type_id)
