b9c4a6582df90f059f14391e2700e29f23d58a3a
Takashi Matsuo <tmatsuo@candit.jp>
2009-09-04 00:15:13 +0900

    Code cleanup.

--- kay/tests/decorator_test.py
+++ kay/tests/decorator_test.py
@@ -8,26 +8,15 @@ Kay decorator test.
 :license: BSD, see LICENSE for more details.
 """
 
-import sys
-import os
 import unittest
 
-from google.appengine.ext import db
 from google.appengine.api import apiproxy_stub_map
-from google.appengine.api import datastore_file_stub
-from google.appengine.api import mail_stub
-from google.appengine.api import urlfetch_stub
-from google.appengine.api.memcache import memcache_stub
-from google.appengine.api import user_service_stub
 from google.appengine.api.capabilities import capability_stub
 
 from werkzeug import BaseResponse, Client, Request
 
 import kay
 from kay.app import get_application
-from kay.utils import local
-from kay.utils import forms
-from kay.utils.forms import ValidationError
 from kay.conf import LazySettings
 from kay.tests import capability_stub as mocked_capability_stub
 
@@ -37,16 +26,16 @@ class DecoratorTestCase(unittest.TestCase):
     s = LazySettings(settings_module='kay.tests.settings')
     app = get_application(settings=s)
     self.client = Client(app, BaseResponse)
+    if apiproxy_stub_map.apiproxy\
+          ._APIProxyStubMap__stub_map.has_key('capability_service'):
+      del(apiproxy_stub_map.apiproxy\
+            ._APIProxyStubMap__stub_map['capability_service'])
 
   def tearDown(self):
     pass
 
   def test_success(self):
     """Test with normal CapabilityServiceStub"""
-    if apiproxy_stub_map.apiproxy\
-          ._APIProxyStubMap__stub_map.has_key('capability_service'):
-      del(apiproxy_stub_map.apiproxy\
-            ._APIProxyStubMap__stub_map['capability_service'])
     apiproxy_stub_map.apiproxy.RegisterStub(
       'capability_service',
       capability_stub.CapabilityServiceStub())
@@ -56,10 +45,6 @@ class DecoratorTestCase(unittest.TestCase):
   def test_failure(self):
     """Test with DisabledCapabilityServiceStub
     """
-    if apiproxy_stub_map.apiproxy\
-          ._APIProxyStubMap__stub_map.has_key('capability_service'):
-      del(apiproxy_stub_map.apiproxy\
-            ._APIProxyStubMap__stub_map['capability_service'])
     apiproxy_stub_map.apiproxy.RegisterStub(
       'capability_service',
       mocked_capability_stub.DisabledCapabilityServiceStub())
