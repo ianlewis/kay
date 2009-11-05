7e59541df7a02d4398af90cb1bdb9d9dfe18b54d
Takashi Matsuo <tmatsuo@candit.jp>
2009-11-05 22:52:09 +0900

    Removed rst2pdf extension from the configration file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,8 @@ kay.setup_env()
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'rst2pdf.pdfbuilder']
+# extensions = ['sphinx.ext.autodoc', 'rst2pdf.pdfbuilder']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
