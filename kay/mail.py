1e1b90c7553b5b318198c739af7a41a017a75aca
Takashi Matsuo <tmatsuo@candit.jp>
2010-01-31 03:14:19 +0900

    Use mail.send_mail_to_admins instead of mail.send_mail when NOTIFY_ERRORS_TO_GAE_ADMINS is set to True. (fixes issue 21)

--- kay/mail.py
+++ kay/mail.py
@@ -17,6 +17,13 @@ def mail_admins(subject, message, fail_silently=False):
   """Sends a message to the admins, as defined by the ADMINS setting."""
   if not settings.ADMINS:
     return
+
+  if settings.NOTIFY_ERRORS_TO_GAE_ADMINS:
+    mail.send_mail_to_admins(sender=settings.ADMINS[0][1],
+                             subject=subject,
+                             body=message)
+    return
+
   for admin in settings.ADMINS:
     mail.send_mail(sender=admin[1],
                    to=admin[1],
