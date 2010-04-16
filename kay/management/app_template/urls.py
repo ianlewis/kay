ddbc1aef1947cf030d75d847bbe327b631ac0d59
Takashi Matsuo <tmatsuo@candit.jp>
2010-04-16 15:36:49 +0900

    Docs.

--- kay/management/app_template/urls.py
+++ kay/management/app_template/urls.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # %app_name%.urls
+# 
+# Following few lines is an example urlmapping with a newer interface.
 
+"""
 from kay.view_group import (
   ViewGroup, URL
 )
@@ -8,3 +11,17 @@ from kay.view_group import (
 view_groups = [
   ViewGroup(URL('/', endpoint='index', view='%app_name%.views.index'))
 ]
+"""
+
+from werkzeug.routing import EndpointPrefix, Rule
+
+def make_rules():
+  return [
+    EndpointPrefix('%app_name%/', [
+      Rule('/', endpoint='index'),
+    ]),
+  ]
+
+all_views = {
+  '%app_name%/index': '%app_name%.views.index',
+}
