bd02dd56a73ff29d81a37acc479b27ea213c7454
Takashi Matsuo <tmatsuo@candit.jp>
2010-05-16 07:27:31 +0900

    Moved to the new url mapping interface.

--- kay/tests/urls.py
+++ kay/tests/urls.py
@@ -7,27 +7,18 @@ Kay test urls.
 :license: BSD, see LICENSE for more details.
 """
 
-from werkzeug.routing import (
-  Map, Rule, Submount,
-  EndpointPrefix, RuleTemplate,
+from kay.routing import (
+  ViewGroup, Rule
 )
-import kay.tests.views
 
-def make_rules():
-  return [
-    EndpointPrefix('tests/', [
-      Rule('/', endpoint='index'),
-      Rule('/index2', endpoint='index2'),
-      Rule('/no_decorator', endpoint='no_decorator'),
-      Rule('/oldpage', endpoint='oldpage', redirect_to='newpage'),
-      Rule('/newpage', endpoint='newpage'),
-    ]),
-  ]
-
-all_views = {
-  'tests/oldpage': kay.tests.views.oldpage,
-  'tests/newpage': kay.tests.views.newpage,
-  'tests/index': kay.tests.views.index,
-  'tests/index2': kay.tests.views.index2,
-  'tests/no_decorator': kay.tests.views.no_decorator,
-}
+view_groups = [
+  ViewGroup(
+    Rule('/', endpoint='index', view='kay.tests.views.index'),
+    Rule('/index2', endpoint='index2', view='kay.tests.views.index2'),
+    Rule('/no_decorator', endpoint='no_decorato',
+         view='kay.tests.views.no_decorator'),
+    Rule('/oldpage', endpoint='oldpage', redirect_to='newpage',
+         view='kay.tests.views.oldpage'),
+    Rule('/newpage', endpoint='newpage', view='kay.tests.views.newpage'),
+  )
+]
